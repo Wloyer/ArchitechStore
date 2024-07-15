c7e0130118cfce1033ce58b6d590d9c20a862ed2
William Loyer <w.loyer@outlook.com>
2024-07-15 22:18:50 +0200

    Registration , Login and logout

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,6 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,17 +21,32 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
+            ->add('firstName', TextType::class, [
+                'label' => 'First Name',
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                    new NotBlank([
+                        'message' => 'Please enter your first name',
+                    ]),
+                ],
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Last Name',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your last name',
+                    ]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your email',
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
+                // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
@@ -43,7 +62,54 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-        ;
+            ->add('phoneNumber', TextType::class, [
+                'label' => 'Phone Number',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your phone number',
+                    ]),
+                ],
+            ])
+            ->add('address', TextType::class, [
+                'label' => 'Address',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your address',
+                    ]),
+                ],
+            ])
+            ->add('zipCode', TextType::class, [
+                'label' => 'Zip Code',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your zip code',
+                    ]),
+                ],
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'City',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your city',
+                    ]),
+                ],
+            ])
+            ->add('country', TextType::class, [
+                'label' => 'Country',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter your country',
+                    ]),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
